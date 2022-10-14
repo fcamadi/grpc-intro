06e4e6372595453614d8b140d748d5de9e4dfed2
francd <fcamadi@gmail.com>
2022-10-14 21:40:27 +0200

    feat: implement server side streaming service

--- src/main/java/org/francd/server/BankService.java
+++ src/main/java/org/francd/server/BankService.java
@@ -1,9 +1,7 @@
 package org.francd.server;
 
 import io.grpc.stub.StreamObserver;
-import org.francd.model.Balance;
-import org.francd.model.BalanceCheckRequest;
-import org.francd.model.BankServiceGrpc;
+import org.francd.model.*;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
@@ -16,6 +14,26 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .build();
 
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withDraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = AccountDBMap.getBalance(accountNumber);
+
+        for (int i = 0; i < (amount / 10); i++) {
+            Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(Money.newBuilder().build());
+            AccountDBMap.deduceBalance(accountNumber, 10);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         responseObserver.onCompleted();
     }
 }
