28511557efb369c1f88fee07f8cbde4a97acb2f1
francd <fcamadi@gmail.com>
2022-10-21 22:15:24 +0200

    test: add test to check exception is thrown

--- src/test/java/org/francd/client/BankClientTest.java
+++ src/test/java/org/francd/client/BankClientTest.java
@@ -2,12 +2,17 @@ package org.francd.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import org.francd.model.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankClientTest {
@@ -36,12 +41,25 @@ public class BankClientTest {
     void withdrawTest() {
         WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
                 .setAccountNumber(7)
-                .setAmount(40)
+                .setAmount(80)
                 .build();
         // Iterator<Money> moneyIterator = bankServiceBlockingStub.withDraw(withdrawRequest);
         bankServiceBlockingStub.withDraw(withdrawRequest)
                 .forEachRemaining(money -> System.out.println("Received: " + money.getValue()));
     }
 
+    @Test
+    void whenWithdrawMoneyAndNotEnoughBalance_ThrowException() {
+        WithdrawRequest withdrawRequest = WithdrawRequest.newBuilder()
+                .setAccountNumber(3)
+                .setAmount(40)
+                .build();
 
+        StatusRuntimeException thrown = assertThrows(
+                StatusRuntimeException.class,
+                () -> bankServiceBlockingStub.withDraw(withdrawRequest)
+                        .forEachRemaining(money -> System.out.println("Received: " + money.getValue())));
+
+        assertTrue(thrown.getMessage().contains("FAILED_PRECONDITION"));
+    }
 }
