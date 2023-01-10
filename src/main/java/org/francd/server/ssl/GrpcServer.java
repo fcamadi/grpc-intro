8982c318663d9f0a9b987431e331f0876455b6ae
francd <fcamadi@gmail.com>
2023-01-10 23:37:18 +0100

    feat: add ssl context in grpc server

--- src/main/java/org/francd/server/ssl/GrpcServer.java
+++ src/main/java/org/francd/server/ssl/GrpcServer.java
@@ -1,16 +1,34 @@
 package org.francd.server.ssl;
 
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import org.francd.server.metadata.MetadataService;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class GrpcServer {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 
-        Server server = ServerBuilder.forPort(6565)
+        URL crtUrl = GrpcServer.class.getClassLoader().getResource("localhost.crt"); // server localhost signed certificate
+        File localhostCrt = new File(crtUrl.toURI());
+        URL pemUrl = GrpcServer.class.getClassLoader().getResource("localhost.pem"); // server localhost key in PKCS8 standard
+        File localhostPem = new File(pemUrl.toURI());
+
+        SslContext sslContext = GrpcSslContexts
+                .configure(
+                    SslContextBuilder.forServer(localhostCrt, localhostPem)
+                )
+                .build();
+
+        Server server = NettyServerBuilder.forPort(6565)
+                .sslContext(sslContext)
                 .addService(new MetadataService())
                 .build();
 
